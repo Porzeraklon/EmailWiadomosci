3569dbadf43a6877dbebeaf400136dea77ba250c
Damian <beldegrin.damian@gmail.com>
2022-04-29 20:14:58 +0200

    zmiany, zmiany...

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, flash, redirect, url_for, render_template, request
+
 
 app = Flask(__name__)
+app.secret_key = "1234"
 
-@app.route("/succes")
-def succes():
-    return render_template("succes.html")
-
-@app.route("/", methods=["POST", "GET"])
+@app.route("/")
 def home():
+    return render_template("index.html")
+
+@app.route("/output", methods=["POST", "GET"])
+def output():
     if request.method == "POST":
         mail = request.form["email"]
         f = open("list.txt", "r")
         for x in f:
             if x.lower() == mail.lower():
-                return render_template("error.html")
+                flash("Błąd! Ten email już jest na liście!")
+                return render_template("index.html")
+            if x != "\n":
+                f = open("list.txt", "a")
+                f.write("\n")
             print(x)
         f = open("list.txt", "a")
-        f.write("\n")
         f.write(mail)
         f.close()
-        return redirect(url_for("succes"))
+        flash("Sukces! Email został dodany do listy!")
+        return render_template("index.html")
 
     else:
         return render_template("index.html")
