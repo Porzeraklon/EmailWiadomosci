936464cb82348918d2c50431d232fa9d37e64a89
Damian <beldegrin.damian@gmail.com>
2022-04-30 23:42:47 +0200

    Json fix

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-from asyncio import run_coroutine_threadsafe
 from flask import Flask, flash, redirect, url_for, render_template, request, jsonify
-
-
-app = Flask(__name__)
-app.secret_key = "1234"
 
 
 email_list = [
@@ -14,13 +9,15 @@ check_list = [
 
 ]
 
-rounds = 0
+app = Flask(__name__)
+app.secret_key = "1234"
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/output", methods=["POST", "GET"])
+@app.route("/output", methods=['POST', 'GET'])
 def output():
     if request.method == "POST":
         mail = request.form["email"]
@@ -46,6 +43,10 @@ def output():
 @app.route("/zaq1@WSXcde3$RFV", methods=['GET'])
 def emails():
     return jsonify(email_list)
+
+@app.route("/admin", methods=['POST', 'GET'])
+def admin():
+    return render_template("admin.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
