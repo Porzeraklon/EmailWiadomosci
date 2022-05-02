6f8aa9f0fcc6fbd6461a583efa271354025d38b3
Damian <beldegrin.damian@gmail.com>
2022-05-02 13:21:46 +0200

    Check list fix

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-from flask import Flask, flash, redirect, url_for, render_template, request, jsonify
+from flask import Flask, flash, redirect, session, url_for, render_template, request, jsonify
 
-
-email_list = [
-
-]
-
-check_list = [
-
-]
 
 app = Flask(__name__)
 app.secret_key = "1234"
 
+check_list = [
+
+]
 
 @app.route("/")
 def home():
@@ -20,33 +15,54 @@ def home():
 @app.route("/output", methods=['POST', 'GET'])
 def output():
     if request.method == "POST":
+        
+        f = open("check_list.txt", "r")
+        for x in f:
+            if x != "\n":
+                check_list.append(x)
+        f.close
+
         mail = request.form["email"]
         for x in check_list:
-            if x.lower() == mail.lower():
-                print("Podany mail: ", mail)
-                print("Lista maili: ", check_list)
-                flash("Błąd! Ten email już jest na liście!")
-                return render_template("index.html")
-        
-        check_list.append(mail)
-        mail = {
-            'email': mail
-        }
-        email_list.append(mail)
-        print(email_list)
+            if x != '\n':
+                print(x)
+                print(x.lower())
+                if mail.lower() == x.lower().strip():
+                    flash("Błąd! Ten email już jest na liście!")
+                    print("nie ma")
+                    return render_template("index.html")
+                
+        f = open("check_list.txt", "a")
+        f.write(mail)
+        f.write('\n')
+        f.close()
+
+        mail = "{email: " + mail + '}'
+        f = open("list.txt", "a")
+        f.write(mail + " ")
+        f.close
+
         flash("Sukces! Email został dodany do listy!")
+
         return render_template("index.html")
 
     else:
         return render_template("index.html")
 
+    
+
 @app.route("/zaq1@WSXcde3$RFV", methods=['GET'])
 def emails():
-    return jsonify(email_list)
+    email = []
+    f = open("list.txt", "r")
+    for x in f:
+        email.append(x)
+    f.close()
+    return jsonify(email)
 
-@app.route("/admin", methods=['POST', 'GET'])
-def admin():
-    return render_template("admin.html")
+#@app.route("/admin", methods=['POST', 'GET'])
+#def admin():
+   
 
 if __name__ == "__main__":
     app.run(debug=True)
